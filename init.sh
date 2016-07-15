a2d5706c9493abcb31dcfe8b92329f1b4840002f
uyamazak <yu_yamazaki@bizocean.co.jp>
2016-07-15 11:46:04 +0900

    make common file

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# you need run this, when git clone
+# create common file hard links
+ln settings.py redis2bq/www/settings.py
+ln utils.py redis2bq/www/utils.py
+ln settings.py oceanus/www/settings.py
+ln utils.py oceanus/www/utils.py
