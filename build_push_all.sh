22766c70ab19d50883ec3496ab73b3608de93087
uyamazak <yu_yamazaki@bizocean.co.jp>
2016-08-09 11:21:30 +0900

    separate build_push.sh bug fix

--- build_push_all.sh
+++ build_push_all.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 sudo docker images | grep swallow | head -5
 sudo docker images | grep redis2bq | head -5
+sudo docker images | grep sub2revelation | head -5
 echo "enter tag"
 
 read tag
@@ -17,10 +18,18 @@ sudo docker build \
         -t asia.gcr.io/oceanus-dev/oceanus-redis2bq:latest \
         /home/BIZOCEAN/yu_yamazaki/project-oceanus/redis2bq
 
+sudo docker build \
+        -t asia.gcr.io/oceanus-dev/oceanus-sub2revelation:$tag \
+        -t asia.gcr.io/oceanus-dev/oceanus-sub2revelation:latest \
+        /home/BIZOCEAN/yu_yamazaki/project-oceanus/sub2revelation
+
 sudo gcloud docker push asia.gcr.io/oceanus-dev/oceanus-swallow:$tag
 sudo gcloud docker push asia.gcr.io/oceanus-dev/oceanus-redis2bq:$tag
+sudo gcloud docker push asia.gcr.io/oceanus-dev/oceanus-sub2revelation:$tag
+
 
 echo "tag: $tag complete"
 echo "please edit, to deploy"
 echo "kc edit deployment oceanus"
 echo "kc edit deployment oceanus-redis2bq"
+echo "kc edit deployment oceanus-sub2revelation"
--- sub2revelation/www/revelation.py
+++ sub2revelation/www/revelation.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone, timedelta
 import os
 import json
 import sys
+import io
 import redis
 import time
 import slack
@@ -12,6 +13,8 @@ from common.utils import oceanus_logging
 from common.settings import (REDIS_HOST, REDIS_PORT,
                              OCEANUS_SITES)
 logger = oceanus_logging()
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
 SLACK_API_TOKEN = os.environ["SLACK_API_TOKEN"]
 SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
 SLACK_BOT_NAME = os.environ["SLACK_BOT_NAME"]
@@ -30,20 +33,49 @@ class revelation:
     def signal_exit_func(self, num, frame):
         if self.keep_processing:
             self.keep_processing = False
-            sys.exit("signal exit")
 
-    def create_message(self, item):
-        json_text = item["data"].decode('utf-8')
-        data = json.loads(json_text, encoding="utf-8")
+    def prepare_messages(self, messages):
+        if not messages:
+            return None
+        return [mess.replace("\u3000", " ") for mess in messages]
+
+    def dict2text(self, dic):
+        text = ""
+        for k, v in sorted(dic.items()):
+            logger.debug("k:{}, v:{}".format(type(k), type(v)))
+            text = "{} {}:{}".format(text, k, v)
+        return text
+
+    def convert2jst(self, dt_str):
+        JST = timezone(timedelta(hours=+9), 'JST')
+        obj_dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S.%f')
+        utc_ts = obj_dt.replace(tzinfo=timezone.utc).timestamp()
+        dt = datetime.fromtimestamp(utc_ts, JST)
+        return dt
+
+    def prepare_item(self, item):
+        channel = item['channel'].decode('utf-8')
+        data = json.loads(item["data"].decode('utf-8'), encoding="utf-8")
         dt = None
         if data["dt"]:
-            JST = timezone(timedelta(hours=+9), 'JST')
-            dt = data["dt"]
-            obj_dt = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S.%f')
-            utc_ts = obj_dt.replace(tzinfo=timezone.utc).timestamp()
-            dt = datetime.fromtimestamp(utc_ts, JST)
+            dt = self.convert2jst(data["dt"])
+        jsn = None
+        if data["jsn"]:
+            jsn = json.loads(data["jsn"])
+            jsn = self.dict2text(jsn)
+        exteded_item = {"data":    data,
+                        "channel": channel,
+                        "dt":      dt,
+                        "jsn":     jsn,
+                        }
+        return exteded_item
 
-        channel = item['channel'].decode('utf-8')
+    def create_messages(self, item):
+        ex_item = self.prepare_item(item)
+        data = ex_item["data"]
+        channel = ex_item["channel"]
+        dt = ex_item["dt"]
+        jsn = ex_item["jsn"]
         messages = []
         if channel == "movieform":
             messages.append("{}\n"
@@ -51,16 +83,19 @@ class revelation:
                             "uid:{}".format(dt, data["uid"]))
 
         if channel == "bizocean":
-            if data["evt"] == "serch_not_found":
+            if data["evt"] == "search_not_found":
+                if not jsn:
+                    logger.error("jsn is None. {}".format(data))
                 messages.append("{}\n"
                                 "書式見つからないキーワード\n"
-                                "kwd:{}".format(dt, data["jsn"]["kwd"]))
+                                "{}".format(dt, jsn))
             if data["tit"] == "bizocean/500":
                 messages.append("{}\n"
-                                "500エラー\n"
+                                "bizoceanで500エラー\n"
                                 "url:{}".format(dt, data["url"]))
 
-        return messages
+        message_list = self.prepare_messages(messages)
+        return message_list
 
     def main(self):
         for s in (SIGINT, SIGTERM):
@@ -74,31 +109,31 @@ class revelation:
         pubsub = self.r.pubsub()
         pubsub.subscribe(self.site_name_list)
         for item in pubsub.listen():
+            logger.debug("for item in pubsub.listen()")
             if not self.keep_processing:
+                logger.debug("unsubscribe()")
                 pubsub.unsubscribe()
-                break
-            if not item:
-                return
+                sys.exit("unsubscribe and sys.exit()")
 
-            if item["type"] == "subscribe":
+            if not item:
+                logger.debug('not item')
                 continue
 
-            logger.debug("channel:{}\n"
-                         "type:{}\n"
-                         "pattern:{}\n"
-                         "data:{}".format(item['channel'],
-                                          item['type'],
-                                          item['pattern'],
-                                          item['data']))
-            message = self.create_message(item)
-            if message:
-                message = "\n".join(message)
+            if item["type"] == "subscribe":
+                logger.debug("type subscribe")
+                continue
+
+            message_list = self.create_messages(item)
+            if message_list:
+                message = "\n".join(message_list).replace("\u3000", " ")
+
                 logger.debug("message:{}".format(message))
                 slack.api_token = SLACK_API_TOKEN
-                slack.chat.post_message(SLACK_CHANNEL,
-                                        message,
-                                        username=SLACK_BOT_NAME)
-            time.sleep(0.1)
+                slack_result = slack.chat.post_message(SLACK_CHANNEL,
+                                                       message,
+                                                       username=SLACK_BOT_NAME)
+                logger.debug("slack_result:{}".format(slack_result))
+            time.sleep(0.05)
 
 if __name__ == '__main__':
     logger.info("starting revelation...")
