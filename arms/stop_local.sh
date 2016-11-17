c0ce39fe7e22c68d5af8003285f55889bb66fa47
uyamazak <yu_yamazaki@bizocean.co.jp>
2016-11-17 18:56:40 +0900

    add stop_local.sh, add description to table schema

---
+++ arms/stop_local.sh
@@ -0,0 +1,1 @@
+sudo docker stop `sudo docker ps -f ancestor=asia.gcr.io/oceanus-dev/arms:latest -q`
---
+++ r2bq/stop_local.sh
@@ -0,0 +1,1 @@
+sudo docker stop `sudo docker ps -f ancestor=asia.gcr.io/oceanus-dev/r2bq:latest -q`
