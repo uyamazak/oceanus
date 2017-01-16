36eca87d39088a2e30eb325f7af077478777d51b
uyamazak <yu_yamazaki@bizocean.co.jp>
2017-01-16 12:18:46 +0900

    delete local files

--- revelation/run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-sudo docker run -dit\
-        -v /home/BIZOCEAN/yu_yamazaki/project-oceanus/revelation/app:/oceanus/app \
-        -e DATA_SET=oceanus_test \
-        -e REDISMASTER_SERVICE_HOST=192.168.2.70 \
-        -e REDISLIST=oceanus_test \
-        -e LOG_LEVEL=DEBUG \
-        -e SPREAD_SHEET_KEY=1n6Yq9JbKFs5PmVi90ebrxaq6tRUQOo2f1BXGWjMoYdY \
-        asia.gcr.io/oceanus-dev/revelation:latest
--- revelation/stop_worker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-sudo docker stop `sudo docker ps -f ancestor=asia.gcr.io/oceanus-dev/revelation:latest -q`
