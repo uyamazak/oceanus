db4341d198a00da27838fdfa8c05c791a8e189d5
uyamazak <yu_yamazaki@bizocean.co.jp>
2017-05-22 11:54:23 +0900

    google pub/subに一本化

--- gopub/app/main.go
+++ gopub/app/main.go
@@ -39,12 +39,19 @@ func main() {
 	projectID := os.Getenv("PROJECT_ID")
 	listenHost := os.Getenv("LISTEN_HOST")
 	listenPort := os.Getenv("LISTEN_PORT")
+	publishMode := os.Getenv("GOPUB_PUBLISH_MODE")
+	if len(publishMode) == 0 {
+		publishMode = "BOTH"
+	}
 	bufferSize, err := strconv.ParseUint(os.Getenv("GOPUB_BUFFER_SIZE"), 10, 64)
 	if err != nil {
 		log.Printf("bufferSize is invalid %v", err)
 		return
 	}
-	topicsMeta, err := pubservice.NewTopicsMeta(os.Getenv("GOPUB_TOPICS"), os.Getenv("GOPUB_TOPIC_NAME_PREFIX"))
+	topicsMeta, err := pubservice.NewTopicsMeta(os.Getenv("GOPUB_TOPICS"),
+		os.Getenv("GOPUB_TOPIC_NAME_PREFIX"),
+		os.Getenv("GOPUB_COMBINED_TOPIC_NAME"),
+		publishMode)
 	if err != nil {
 		log.Printf("Failed to create topicsMeta %v", err)
 		return
@@ -52,17 +59,21 @@ func main() {
 	log.Printf("GOPUB Server start\n"+
 		"GOMAXPROCS:              %v\n"+
 		"projectID:               %v\n"+
+		"publishMode:             %v\n"+
 		"topicsMeta.Names:        %v\n"+
 		"topicsMeta.ConfigString: %v\n"+
 		"topicsMeta.Count:        %v\n"+
+		"topicsMeta.CombinedName: %v\n"+
 		"listenHost:              %v\n"+
 		"listenPort:              %v\n"+
 		"bufferSize:              %v",
 		runtime.GOMAXPROCS(0),
 		projectID,
+		publishMode,
 		topicsMeta.Names,
 		topicsMeta.ConfigString,
 		topicsMeta.Count,
+		topicsMeta.CombinedName,
 		listenHost,
 		listenPort,
 		bufferSize)
@@ -86,6 +97,14 @@ func main() {
 			log.Printf("Failed to accept %v", err)
 			return
 		}
-		connectionHandler(conn, bufferSize, service.PublishHandler)
+
+		switch {
+		case publishMode == "COMBINED":
+			connectionHandler(conn, bufferSize, service.CombinedPublishHandler)
+		case publishMode == "INDIVIDUAL":
+			connectionHandler(conn, bufferSize, service.IndividualPublishHandler)
+		case publishMode == "BOTH":
+			connectionHandler(conn, bufferSize, service.PublishHandler)
+		}
 	}
 }
